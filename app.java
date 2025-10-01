9b40fe0f93dfd81be2db45c7baa30fc2b4c8b179
Alba Muñoz Ordoño <amu2760@proven.local>
2025-10-01 16:49:59 +0200

    Creating Alba class in alba.java and implementing method printAlba() in app.java

--- app.java
+++ app.java
@@ -1,5 +1,9 @@
+import alba.java
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    	Alba miAlba = new Alba();
+        miAlba.printAlba();
     }
 }
