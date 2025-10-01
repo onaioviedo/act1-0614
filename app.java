fe3f0bb2aa3d19e156283ca36107014140f1153b
onaioviedo <oviedo.onai@gmail.com>
2025-10-01 16:59:55 +0200

    cambios app onai

--- app.java
+++ app.java
@@ -1,9 +1,11 @@
 import alba.java
-
+import onai.java
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     	Alba miAlba = new Alba();
         miAlba.printAlba();
+	Onai onai = new Onai();
+	onai.Onai();
     }
 }
